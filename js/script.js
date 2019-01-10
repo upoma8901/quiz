9110d9d6f73a273009816f48ada2b18127828e41
Upoma bhuiyan <upomab8901@queenstechhs.org>
2019-01-10 23:04:31 +0000

    d3

--- js/script.js
+++ js/script.js
@@ -8,14 +8,16 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        
+         alert(q1Result);
     });
 //score('Creative')
 
 
 //console.log(totalScore(score));
 // This is our function box
-function score(q1Result,q2Result,q3Result) { 
+
+ 
+ function ans1(q1Result){
     if(q1Result==="Creative"){
         return 5;
     }
@@ -28,28 +30,79 @@ function score(q1Result,q2Result,q3Result) {
      else if(q1Result==="Stress-Free"){
         return 25;
     }
-     if(q2Result==="Reading"){
+     
+ }
+    function ans2(q2Result){
+    if(q2Result==="Reading"){
         return 5;
+    }
+    else if(q2Result==="Sleeping"){
+        return 10;
     }
     else if(q2Result==="Gaming"){
-        return 10;
-    }
-    else if(q2Result==="Cleaning"){
         return 20;
     }
-     else if(q2Result==="Sleeping"){
+     else if(q2Result==="Cleaning"){
         return 25;
     }
-     if(q3Result==="Reading"){
+    }
+    
+    function ans2(q2Result){
+
+     if(q3Result==="Summer"){
         return 5;
     }
-    else if(q3Result==="Gaming"){
+    else if(q3Result==="Winter"){
         return 10;
     }
-    else if(q3Result==="Cleaning"){
+     else if(q3Result==="Fall"){
         return 20;
     }
-     else if(q3Result==="Sleeping"){
+    else if(q3Result==="Spring"){
         return 25;
     }
+    }
+    
+    
+    
+    function ans4(q4Result){ 
+        if(q4Result==="Horror"){
+        return 5;
+    }
+    else if(q4Result==="Sci-fic"){
+        return 10;
+    }
+    else if(q4Result==="Fantasy"){
+        return 20;
+    }
+     else if(q4Result==="Comedy"){
+        return 25;}
+    }
+
+var result=$(".result");
+var h;
+var h=q1Result+q2Result+q3Result+q4Result;
+if (h>20 ||h===20 && h<25|| h===25){
+  result="chipmunk"   }
+
+
+else if (h>25  && h<35|| h===35){
+  result="owl"   }
+  
+  else if (h>35  && h<45|| h===45){
+  result="koala"   }
+  
+ else if ( h>45  && h<65|| h===65){
+  result="bear"   }
+  
+  else if ( h>65  && h<75|| h===75){
+  result="seahorse"   }
+  
+  else if ( h>75  && h<85|| h===85){
+  result="penguin"   }
+  
+  else if ( h>85  && h<100|| h===100){
+  result="seahorse"   }
+  
+}
 
